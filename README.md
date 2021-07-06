cb1925237415b1b3fe53eea0eda85872b28df78a
Helmut Kemper <helmut.kemper@gmail.com>
2021-07-06 10:57:56 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# benchmarkCRC
+# benchmark for CRC algorithm
+
+```shell
+goos: darwin
+goarch: arm64
+BenchmarkAdler32
+BenchmarkAdler32-8           	23755016	        51.34 ns/op
+BenchmarkCRC32IEEE
+BenchmarkCRC32IEEE-8         	39377883	        30.44 ns/op
+BenchmarkCRC31Castagnoli
+BenchmarkCRC31Castagnoli-8   	39839700	        30.02 ns/op
+BenchmarkCRC31Koopman
+BenchmarkCRC31Koopman-8      	 4124380	       289.9 ns/op
+BenchmarkCRC64ISO
+BenchmarkCRC64ISO-8          	 9016206	       134.1 ns/op
+BenchmarkCRC64ECMA
+BenchmarkCRC64ECMA-8         	 8886650	       133.4 ns/op
+BenchmarkFNV32
+BenchmarkFNV32-8             	 8146906	       145.2 ns/op
+BenchmarkFNV32a
+BenchmarkFNV32a-8            	 8193144	       145.7 ns/op
+BenchmarkFNV64
+BenchmarkFNV64-8             	 8198060	       145.5 ns/op
+BenchmarkFNV64a
+BenchmarkFNV64a-8            	 8174168	       145.3 ns/op
+BenchmarkHmacSha256
+BenchmarkHmacSha256-8        	 5323273	       225.3 ns/op
+BenchmarkHmacMd5
+BenchmarkHmacMd5-8           	 2315974	       517.9 ns/op
+BenchmarkHmacSha1
+BenchmarkHmacSha1-8          	 5201324	       230.4 ns/op
+BenchmarkHmacSha512
+BenchmarkHmacSha512-8        	  620304	      1825 ns/op
+BenchmarkMd5
+BenchmarkMd5-8               	 3869907	       307.0 ns/op
+BenchmarkMd52
+BenchmarkMd52-8              	 1666381	       718.6 ns/op
+BenchmarkSha1
+BenchmarkSha1-8              	 9310050	       128.1 ns/op
+BenchmarkSha254
+BenchmarkSha254-8            	 9437179	       126.1 ns/op
+BenchmarkSha256
+BenchmarkSha256-8            	 9391630	       126.0 ns/op
+BenchmarkSha512
+BenchmarkSha512-8            	 1295797	       926.9 ns/op
+```
---
+++ cdc_md5_test.go
@@ -0,0 +1,428 @@
+package crc_md5
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/json"
+	md52 "github.com/golang/go/src/pkg/crypto/md5"
+	"hash/adler32"
+	"hash/crc32"
+	"hash/crc64"
+	"hash/fnv"
+	"testing"
+)
+
+type User struct {
+	Name       string
+	FamilyName string
+	Gener      string
+}
+
+type Address struct {
+	Street string
+	Number int
+}
+
+type test struct {
+	User    User
+	Address Address
+}
+
+var user = test{
+	User: User{
+		Name:       "Dino",
+		FamilyName: "Sauro",
+		Gener:      "T-Rex",
+	},
+	Address: Address{
+		Street: "Pagea Street",
+		Number: 1000,
+	},
+}
+
+func BenchmarkAdler32(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := adler32.New()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkCRC32IEEE(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := crc32.New(crc32.MakeTable(crc32.IEEE))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkCRC31Castagnoli(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := crc32.New(crc32.MakeTable(crc32.Castagnoli))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkCRC31Koopman(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := crc32.New(crc32.MakeTable(crc32.Koopman))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkCRC64ISO(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := crc64.New(crc64.MakeTable(crc64.ISO))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkCRC64ECMA(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := crc64.New(crc64.MakeTable(crc64.ECMA))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkFNV32(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := fnv.New32()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkFNV32a(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := fnv.New32a()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkFNV64(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := fnv.New64()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkFNV64a(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := fnv.New64a()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkHmacSha256(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	key := "SHA-256"
+	hash := hmac.New(sha256.New, []byte(key))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkHmacMd5(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	key := "123"
+	hash := hmac.New(md5.New, []byte(key))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkHmacSha1(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	key := "123"
+	hash := hmac.New(sha1.New, []byte(key))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkHmacSha512(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	key := "123"
+	hash := hmac.New(sha512.New, []byte(key))
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkMd5(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := md5.New()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkMd52(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := md52.New()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkSha1(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := sha1.New()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkSha254(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := sha256.New224()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkSha256(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := sha256.New()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
+
+func BenchmarkSha512(b *testing.B) {
+	var err error
+	var data []byte
+	data, err = json.Marshal(&user)
+	if err != nil {
+		b.FailNow()
+	}
+
+	hash := sha512.New384()
+
+	for i := 0; i < b.N; i++ {
+		_, err = hash.Write(data)
+		if err != nil {
+			b.FailNow()
+		}
+		hash.Sum(nil)
+	}
+}
